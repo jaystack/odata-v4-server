675c4b0dc4a04b8a850d873e2351ef1547a801b4
Lázár Viktor <lazarv1982@gmail.com>
2017-06-02 15:47:18 +0200

    Handle processor resolve base type error

--- src/lib/processor.ts
+++ src/lib/processor.ts
@@ -1054,10 +1054,15 @@ export class ODataProcessor extends Transform{
             if (typeof body["@odata.type"] == "function") elementType = body["@odata.type"];
             let keys = Edm.getKeyProperties(elementType);
             let resolveBaseType = (elementType) => {
-                let baseType = Object.getPrototypeOf(elementType.prototype).constructor;
-                if (baseType != Object && Edm.getProperties(baseType.prototype).length > 0){
-                    keys = Edm.getKeyProperties(baseType).concat(keys);
-                    resolveBaseType(baseType);
+                if (elementType && elementType.prototype){
+                    let proto = Object.getPrototypeOf(elementType.prototype);
+                    if (proto){
+                        let baseType = proto.constructor;
+                        if (baseType != Object && Edm.getProperties(baseType.prototype).length > 0){
+                            keys = Edm.getKeyProperties(baseType).concat(keys);
+                            resolveBaseType(baseType);
+                        }
+                    }
                 }
             };
             resolveBaseType(elementType);
@@ -1160,11 +1165,16 @@ export class ODataProcessor extends Transform{
         }
         let ctrl = this.serverType.getController(elementType);
         let resolveBaseType = (elementType) => {
-            let baseType = Object.getPrototypeOf(elementType.prototype).constructor;
-            if (baseType != Object && Edm.getProperties(baseType.prototype).length > 0){
-                props = Edm.getProperties(baseType.prototype).concat(props);
-                ctrl = ctrl || this.serverType.getController(baseType);
-                resolveBaseType(baseType);
+            if (elementType && elementType.prototype){
+                let proto = Object.getPrototypeOf(elementType.prototype);
+                if (proto){
+                    let baseType = proto.constructor;
+                    if (baseType != Object && Edm.getProperties(baseType.prototype).length > 0){
+                        props = Edm.getProperties(baseType.prototype).concat(props);
+                        ctrl = ctrl || this.serverType.getController(baseType);
+                        resolveBaseType(baseType);
+                    }
+                }
             }
         };
         resolveBaseType(elementType);
