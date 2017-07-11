10d2a8ff9dffa1d16b0acb80dac48707be1bf359
slajher <slajher92@gmail.com>
2017-07-11 14:36:54 +0200

    add stream createTest function

--- src/test/stream.spec.ts
+++ src/test/stream.spec.ts
@@ -1,1 +1,34 @@
-// stream
+/// <reference types="mocha" />
+import { ODataServer } from "../lib/index";
+import { testFactory } from './server.spec'
+const { expect } = require("chai");
+
+function createTest(testcase: string, server: typeof ODataServer, command: string, compare: any, body?: any) {
+    let test = command.split(" ");
+    let method = test[0].toLowerCase();
+    let path = test.slice(1).join(" ");
+    let testServer = new server();
+    it(`${testcase} (${command})`, () => {
+
+        let context: any = {};
+        context.url = path;
+        context.method = method;
+        context.body = body;
+
+        return new Promise((resolve, reject) => {
+            testServer.write(context);
+            testServer.on("data", data => {
+                resolve(data)
+            })
+            testServer.on("error", err => {
+                reject(err)
+            })
+        }).then(result => {
+            expect(result).to.deep.equal(compare);
+        })
+    });
+}
+
+describe("Odata stream", () => {
+    testFactory(createTest);
+});
